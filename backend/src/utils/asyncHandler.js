fb281fa594f33d3c5e77e848226a4bdeab1e1162
jhrhq <jhr.code@proton.me>
2026-08-27 08:19:30 +0600

    fix(backend): syntax error in asyncHandler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asyncHandler = async (requestHandler) => (req, res, next) => {
-  return Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
 };
 
 export { asyncHandler };
